54b7afaa7b7fd339abeb266f4cd5fe969a860c84
Trenton Wuerker <trenton.wuerker@gmail.com>
2017-06-12 16:10:22 -0600

    finished assignment

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 // Your JS goes here
+
+function checkboard(){
+  for (i=0; i<81; i++) {
+    if (i % 2) {
+      var div = document.createElement('div')
+      document.body.appendChild(div)
+      div.style.paddingBottom = '11.1%'
+      div.style.width = '11.1%'
+      div.style.float = 'left'
+      div.style.backgroundColor = 'black'
+    } else {
+      var div = document.createElement('div')
+      document.body.appendChild(div)
+      div.style.paddingBottom = '11.1%'
+      div.style.width = '11.1%'
+      div.style.float = 'left'
+      div.style.backgroundColor = 'red'
+    }
+  }
+}
+
+checkboard()
