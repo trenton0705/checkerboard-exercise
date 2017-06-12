137f9dfaebc051e8f83725e19fea6b06caf18e46
Trenton Wuerker <trenton.wuerker@gmail.com>
2017-06-12 16:51:38 -0600

    random colors

--- script.js
+++ script.js
@@ -2,21 +2,15 @@
 
 function checkboard(){
   for (i=0; i<81; i++) {
-    if (i % 2) {
-      var div = document.createElement('div')
-      document.body.appendChild(div)
-      div.style.paddingBottom = '11.1%'
-      div.style.width = '11.1%'
-      div.style.float = 'left'
-      div.style.backgroundColor = 'black'
-    } else {
-      var div = document.createElement('div')
-      document.body.appendChild(div)
-      div.style.paddingBottom = '11.1%'
-      div.style.width = '11.1%'
-      div.style.float = 'left'
-      div.style.backgroundColor = 'red'
-    }
+    var color1 = Math.floor(Math.random()*256)
+    var color2 = Math.floor(Math.random()*256)
+    var color3 = Math.floor(Math.random()*256)
+    var div = document.createElement('div')
+    document.body.appendChild(div)
+    div.style.paddingBottom = '11.1%'
+    div.style.width = '11.1%'
+    div.style.float = 'left'
+    div.style.backgroundColor = 'rgb' + '(' + color1 + ', ' + color2 + ', ' + color3 + ')'
   }
 }
 
