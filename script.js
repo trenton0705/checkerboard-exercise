425f735341efcdf1efd6d1dbcf77803ed7a65240
Trenton Wuerker <trenton.wuerker@gmail.com>
2017-06-13 10:28:40 -0600

    added gradient

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 // Your JS goes here
 
 function checkboard(){
+
   for (i=0; i<81; i++) {
-    var color1 = Math.floor(Math.random()*256)
-    var color2 = Math.floor(Math.random()*256)
-    var color3 = Math.floor(Math.random()*256)
-    var div = document.createElement('div')
-    document.body.appendChild(div)
-    div.style.paddingBottom = '11.1%'
-    div.style.width = '11.1%'
-    div.style.float = 'left'
-    div.style.backgroundColor = 'rgb' + '(' + color1 + ', ' + color2 + ', ' + color3 + ')'
+    if (i % 2) {
+      var div = document.createElement('div')
+      document.body.appendChild(div)
+      div.style.paddingBottom = '11.1%'
+      div.style.width = '11.1%'
+      div.style.float = 'left'
+      div.style.background = 'linear-gradient(red, yellow)'
+      div.style.backgroundAttachment = 'fixed'
+    } else {
+      var div = document.createElement('div')
+      document.body.appendChild(div)
+      div.style.paddingBottom = '11.1%'
+      div.style.width = '11.1%'
+      div.style.float = 'left'
+      div.style.background = 'linear-gradient(blue, green)'
+      div.style.backgroundAttachment = 'fixed'
+    }
   }
 }
 
